04cf49b8c8f994bf1929519bb8c9ea700010cd73
iqbal <asifmai@hotmail.com>
2019-05-19 16:07:08 +0500

    udpate

--- scrape.js
+++ scrape.js
@@ -1,5 +1,6 @@
 const metacriticscrape = require('./scrapemetacritic');
 const psencrape1 = require('./scrapepsen1');
+const psencrape2 = require('./scrapepsen2');
 
 run();
 
@@ -8,5 +9,8 @@ async function run () {
   // await metacriticscrape();
 
   // Scrape Games URLs from Playstation website with exact title match, Full Game tag and PS4 tag
-  await psencrape1();
+  // await psencrape1();
+
+  // Scrape Games URLs from Playstation website with exact title match, PS VR Game tag or PSN Game and PS4 tag
+  await psencrape2();
 }
--- test.js
+++ test.js
@@ -2,7 +2,16 @@ const fs = require('fs');
 
 const fl = JSON.parse(fs.readFileSync('psen1.json'));
 
+let filled = 0;
+let nonfilled = 0;
+    
 for (let index = 0; index < fl.length; index++) {
-  if (!fl.url) console.log(index);
-  
+  if (fl[index].url) {
+    filled = filled + 1
+  } else {
+    nonfilled = nonfilled + 1
+  }
 }
+console.log(`Number of Games: ${fl.length}`)
+console.log(`Filled with urls: ${filled}`)
+console.log(`Not filled with urls: ${nonfilled}`)
